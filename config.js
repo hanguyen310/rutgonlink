b12685127640fabdb14d3006ec36acad2b00ebe6
hanguyen310 <88221316+hanguyen310@users.noreply.github.com>
2021-08-09 14:49:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken:'1799632301:AAE_iRUbS51yy9ZetW38vEqrBGXbZqof6DE',
+    botToken:'1920938840:AAGbauM9TG90bSM4w3-7Qphwr8SglJ-1PYA',
     bitlyToken:'cb492cbdcb943468faca78e5803e7c262d8e0970'
 };
